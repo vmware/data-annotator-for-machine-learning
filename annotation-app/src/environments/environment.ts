f376dfdf163d14d19d13dccd81fa6dad3e029c08
rekey <43572159+rekey136015670@users.noreply.github.com>
2021-01-28 09:24:39 +0800

    Update environment.ts

--- annotation-app/src/environments/environment.ts
+++ annotation-app/src/environments/environment.ts
@@ -7,9 +7,9 @@ SPDX-License-Identifier: Apache-2.0
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `angular-cli.json`.
 
+import { Env } from 'app/model/index';
 
-
-export const environment = {
+export const environment:Env = {
   production: false
 };
 
