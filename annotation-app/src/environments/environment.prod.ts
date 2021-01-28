5c39c342c6d12695b7b6cfca5239491a79f049c6
rekey <43572159+rekey136015670@users.noreply.github.com>
2021-01-28 09:25:16 +0800

    Update environment.prod.ts

--- annotation-app/src/environments/environment.prod.ts
+++ annotation-app/src/environments/environment.prod.ts
@@ -2,8 +2,9 @@
 Copyright 2019-2021 VMware, Inc.
 SPDX-License-Identifier: Apache-2.0
 */
+import { Env } from 'app/model/index';
 
-export const environment = {
+export const environment:Env = {
   production: true
 };
 
