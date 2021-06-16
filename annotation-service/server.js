45ae4401531ad8faaa3662f7d0947986a80541de
Kai-Qiao <joe.mikey.k@gmail.com>
2021-06-16 16:52:47 +0800

    update res access control

--- annotation-service/server.js
+++ annotation-service/server.js
@@ -38,6 +38,7 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "*");
   res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Expose-Headers", "*");
   next();
 });
 
