d1b36b3d30a2adb5d1cd99322c67881ef1380f96
yangrong16 <18710323116@163.com>
2022-03-01 17:08:23 +0800

    modify e2e for sandbox env

--- page-object/new-project-page.ts
+++ page-object/new-project-page.ts
@@ -224,9 +224,13 @@ export class NewProjectPage extends CommonPage {
 
   async setDataSubmit() {
     if (process.env.IN) {
+      console.log('start to show set_data btn');
+      await browser.sleep(2000);
       await FunctionUtil.elementVisibilityOf(this.SET_DATA_BTN);
       await browser.actions().mouseMove(this.SET_DATA_BTN).perform();
+      console.log('shown set_data btn');
     }
+    console.log('start to click set_data btn');
     await this.SET_DATA_BTN.click();
     await ExpectedConditions.invisibilityOf($(".btn.uploadLoading"));
   }
