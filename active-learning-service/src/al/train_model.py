9c03201b3d2bf62cab93c7066a27340582e25749
Joe K Qiao <joe.mikey.k@gmail.com>
2022-03-08 10:25:41 +0800

    support #21 (#250)
    * Enable user selection of active learning query strategy (#249)
    * support chose active-learning query strategy

--- active-learning-service/src/al/train_model.py
+++ active-learning-service/src/al/train_model.py
@@ -6,16 +6,20 @@ import time
 import numpy as np
 import pickle
 
+from config.constant import cst
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.neighbors import KNeighborsClassifier
 from modAL.models import ActiveLearner
+from functools import partial
+from modAL.batch import uncertainty_batch_sampling
+from modAL.uncertainty import uncertainty_sampling, margin_sampling, entropy_sampling
 
 modelDir = "models/"
 log = logging.getLogger('loop_al')
 
 
-def train_model(data, project_id, estimator):
+def train_model(data, project_id, estimator, query_strategy):
     log.info(f'train model')
     start_time = time.time()
 
@@ -23,13 +27,24 @@ def train_model(data, project_id, estimator):
     y_test = data['y_test']
 
     # initial model train model
-    if(estimator == 'KNC'):
+    if estimator == cst['ESTIMATOR']['KNC']:
         estimator = KNeighborsClassifier()
-    elif(estimator == 'GBC'):
+    elif estimator == cst['ESTIMATOR']['GBC']:
         estimator = GradientBoostingClassifier()
-    elif(estimator == 'RFC'):
+    elif estimator == cst['ESTIMATOR']['RFC']:
         estimator = RandomForestClassifier()
-    learner = ActiveLearner(estimator=estimator, X_training=data['x_teach'], y_training=data['y_teach'])
+
+    if query_strategy == cst['QUERY_STRATEGY']['POOL_BASED_SAMPLING']['PB_UNS']:
+        query_strategy = uncertainty_sampling
+    elif query_strategy == cst['QUERY_STRATEGY']['POOL_BASED_SAMPLING']['PB_MS']:
+        query_strategy = margin_sampling
+    elif query_strategy == cst['QUERY_STRATEGY']['POOL_BASED_SAMPLING']['PB_ES']:
+        query_strategy = entropy_sampling
+    elif query_strategy == cst['QUERY_STRATEGY']['RANKED_BATCH_MODE']['RBM_UNBS']:
+        batch_size = 10
+        query_strategy = partial(uncertainty_batch_sampling, n_instances=batch_size)
+
+    learner = ActiveLearner(estimator=estimator, query_strategy=query_strategy, X_training=data['x_teach'], y_training=data['y_teach'])
 
     # save the model to disk
     model_name = project_id + "_model.pkl"
