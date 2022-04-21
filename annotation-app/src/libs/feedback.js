e5e59f4795729f118a36db30d8d67f591fb60e96
rekey <43572159+zrekey@users.noreply.github.com>
2022-04-21 11:28:03 +0800

    Migrate to lumos ipf (#269)
    * alert unsubscribe status
    
    * migrate to lumos ipf

--- annotation-app/src/libs/feedback.js
+++ annotation-app/src/libs/feedback.js
@@ -3,37 +3,30 @@ Copyright 2019-2021 VMware, Inc.
 SPDX-License-Identifier: Apache-2.0
 */
 
+
 var common = {
-  esp: null,
+  lumos: null,
   feedback: (url) => {
+    window["_paq"] = window["_paq"] || [];
     (function (a, b, c, d, e, f) {
       window[d] = window[d] || {};
       window[d]._q = window[d]._q || [];
-      ['identify', 'page', 'track', 'metadata'].forEach(function (t) {
+      ['identify', 'page', 'track', 'metadata', 'locale', 'theme'].forEach(function (t) {
         window[d][t] = function () {
-          var e = Array.prototype.slice.call(arguments);
-          e.unshift(t);
-          window[d]._q.push(e);
-        };
+          var e = Array.prototype.slice.call(arguments); e.unshift(t); window[d]._q.push(e);
+        }
       });
-      window[d].init =
-        window[d].init ||
-        function (c, n, s) {
-          window[d]._clientId = c;
-          window[d]._clientName = n;
-          window[d]._configs = s || {};
-        };
-
       window[d]._url = c;
-      let eParam = e;
-      eParam = a.createElement(b);
-      eParam.src = c;
-      eParam.async = 1;
-      let fParam = f;
-      fParam = a.getElementsByTagName(b)[0];
-      fParam.parentNode.insertBefore(eParam, fParam);
-    })(document, 'script', url, 'esp');
-    this.common.esp = esp;
-    return esp;
+      window[d].init = window[d].init || function (p, q, r) {
+        window[d]._clientId = p; window[d]._clientName = q; window[d]._configs = r || {};
+        e = a.createElement(b); e.async = 1; e.src = c + '?client_id=' + p + '&namespace=' + d;
+        f = a.getElementsByTagName(b)[0]; f.parentNode.insertBefore(e, f);
+      };
+    })(document, 'script', url, 'lumos');
+
+    this.common.lumos = lumos;
+    return lumos;
   },
 };
+
+
